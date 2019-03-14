b00bc40ad50e627bfb5723792ddcf6c49506573e
miguel molina <miguel.molina@erni-espana.es>
2019-03-14 17:56:34 +0100

    First Call to random number method success

---
+++ IotRandomWinner/IotRandomWinner.Server/Controllers/SampleDataController.cs
@@ -0,0 +1,39 @@
+﻿using IotRandomWinner.Server.iot;
+using IotRandomWinner.Shared;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IotRandomWinner.Server.Controllers
+{
+    [Route("api/[controller]")]
+    public class SampleDataController : Controller
+    {
+        public int MAX_COUNT_OF_DEVICES = 100;
+        public string CONNECTION_STRING = "HostName=ERNI-IOT-EDGE-HUB.azure-devices.net;SharedAccessKeyName=mxchipgif;SharedAccessKey=5HDv1uo1F4UinBnBaF/Y9GJ0R+mzLT1CWXakfDYOrcA=";
+        private readonly DevicesProcessor devicesProcessor;
+
+        public SampleDataController()
+        {
+            this.devicesProcessor = new DevicesProcessor(CONNECTION_STRING, MAX_COUNT_OF_DEVICES, string.Empty);
+        }
+
+        [HttpGet("[action]")]
+        public IEnumerable<DeviceEntity> WeatherForecasts()
+        {
+            var rng = new Random();
+            var devices = this.devicesProcessor.GetDevices().Result;
+            return devices;
+        }
+
+        [HttpPost("[action]")]
+        public async Task InvokeDeviceMethodAsync([FromBody] RandomItemRequest method)
+        {
+            var rng = new Random();
+            await this.devicesProcessor.InvokeDeviceMethodAsync(method.DeviceId.FirstOrDefault(), method.Method);
+            
+        }
+    }
+}
---
+++ IotRandomWinner/IotRandomWinner.Shared/RandomItemRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+namespace IotRandomWinner.Shared
+{
+    public class RandomItemRequest
+    {
+        public string Method { get; set; }
+        public List<DeviceEntity> DeviceId { get; set; }
+
+    }
+}
